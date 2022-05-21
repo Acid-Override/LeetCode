f75ea9cb808bd14a0c9cb73692dab5820534bbbf
Will Atwood <X1@galaxia.us>
2022-05-21 16:54:26 -0400

    Time: 68 ms (79.54%), Space: 43.5 MB (54.24%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
@@ -3,14 +3,10 @@
  * @return {number}
  */
  
-var arraySign = function(nums) {
-
+ var arraySign = function(nums) {
+   if(nums.includes(0)) {
+     return 0
+   }
   let prod = nums.reduce((a, b) => a * b)
-  return  signFunc(prod)
-
+  return prod > 0 ? 1 : prod < 0 ? -1 : 0
 };
-
-var signFunc = function (num) {
-
-  return num > 0 ? 1 : num < 0 ? -1 : 0
-}
