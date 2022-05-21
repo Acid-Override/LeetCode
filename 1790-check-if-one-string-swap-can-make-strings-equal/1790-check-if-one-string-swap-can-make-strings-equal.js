4fc8a03a0e408a5ffcd00e42bbd96eca7634ab68
Will Atwood <X1@galaxia.us>
2022-05-21 19:16:55 -0400

    Time: 66 ms (76.08%), Space: 42.3 MB (57.97%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+  var areAlmostEqual = function(s1, s2) {
+   debugger
+   if (s1.length !== s2.length) {
+     return false
+   }
+
+  if (JSON.stringify(s1) === JSON.stringify(s2)) {
+    return true
+  }
+
+  let cache = {}
+
+  for (let i = 0; i < s1.length; i++ ) {
+    if ( s1[i] === s2[i]) {
+      continue
+    }
+    if (s1[i] !== s2[i] && !cache[s1[i]] ) {
+      cache[s1[i]] = s2[i]
+      continue
+    }
+    return false
+  }
+  if (Object.keys(cache).length > 2) {
+    return false
+  }
+
+  for (let key in cache) {
+    let temp = cache[key] // cache[b] = k  cache[k] = b
+    if (cache[temp] === key) {
+      return true
+    }
+  }
+return false
+};
