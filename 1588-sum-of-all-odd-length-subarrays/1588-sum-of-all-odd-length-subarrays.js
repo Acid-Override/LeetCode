bb77efcc5ed760169a860db8afb4acb6c1ce1695
Will Atwood <X1@galaxia.us>
2022-05-22 18:24:17 -0400

    Time: 109 ms (27.14%), Space: 48.7 MB (11.95%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.js
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.js
@@ -3,18 +3,16 @@
  * @return {number}
  */
  var sumOddLengthSubarrays = function(arr) {
-
-  let accArr = []
-  let sum = 0
-
+ let sum = 0
   for ( let i = 0; i < arr.length; i++ ) {
     for ( let j = arr.length; j >= 0 && j > i; j-- ) {
       let curSlice = arr.slice(i, j)
       if ( curSlice.length % 2 === 1) {
-        curSlice.forEach(num => sum += num)
+        for (let i = 0; i < curSlice.length; i++ ) {
+            sum += curSlice[i]
+        }
       }
     }
   }
-//return accArr.flat().reduce((a, b) => a + b)
 return sum
 };
