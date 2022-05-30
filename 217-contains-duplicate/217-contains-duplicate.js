a7d2afae3ef7565c4f7744d0ad0a80c3c3923cf3
Will Atwood <X1@galaxia.us>
2022-05-31 00:42:48 +0200

    Time: 98 ms (76.96%), Space: 50.8 MB (59.28%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,12 +3,6 @@
  * @return {boolean}
  */
  var containsDuplicate = function(nums) {
-
-  for ( let i = 0; i < nums.length; i++ ) {
-    if ( nums.lastIndexOf(nums[i]) !== i ) {
-      return true
-    }
-  }
-  return false
-
+ newNums = new Set(nums)
+return !(newNums.size === nums.length)
 };
