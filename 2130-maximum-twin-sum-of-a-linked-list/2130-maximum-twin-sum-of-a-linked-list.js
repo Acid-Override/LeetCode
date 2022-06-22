d72c8f46b313caf06daf11c1a1e9c56e8bcd9eeb
Will Atwood <X1@galaxia.us>
2022-06-22 10:59:41 -0400

    Time: 179 ms (88.54%), Space: 76.1 MB (75.89%) - LeetHub

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+ var pairSum = function(head) {
+  let arr = []
+  let maxPair = 0
+  let pair;
+
+  while ( head ) {
+    arr.push(head.val)
+    head = head.next
+  }
+   for ( let i = 0; i <= arr.length/2; i++ ) {
+    pair = arr[i] + arr[arr.length - 1 - i]
+    if ( pair > maxPair ) {
+      maxPair = pair
+    }
+   }
+   return maxPair
+};
