8b7946566dc87002f706a0458522154b6a7d67d4
Will Atwood <X1@galaxia.us>
2022-07-01 21:56:30 -0400

    Time: 91 ms (88.40%), Space: 48.1 MB (59.40%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function(s) {
+    
+    let obj = {}
+    
+    for ( let i = 0; i < s.length; i++ ) {
+        obj[s[i]] = (obj[s[i]] || 0 ) + 1
+    }
+    
+    let arr = []
+    
+    
+    
+    for ( let key in obj ) {
+        arr.push({key, val: obj[key]})
+    }
+    return arr.sort((a, b) => b.val - a.val)
+    .map(ea => new Array(ea.val).fill(ea.key).join(''))
+    .join('')
+    
+   
+};
